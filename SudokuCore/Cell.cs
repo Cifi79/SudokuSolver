4f203e10193c6559653d2dbe6108ce98c7456407
Ricky F <cifi79@hotmail.com>
2020-05-19 18:22:32 +0200

    First commit -create structure of elements that compose the board

---
+++ SudokuCore/Cell.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuCore.Elements
+{
+    public class Cell
+    {
+        private int dimension = 9;
+
+        public int Dimension
+        {
+            get { return dimension; }
+            set
+            {
+
+
+                dimension = value;
+                RefreshDimension();
+            }
+        }
+
+        /// <summary>
+        /// This read-only property return the value of a cell if it is sure
+        /// Otherwise return -1
+        /// </summary>
+        public int CorrectValue
+        {
+            get
+            {
+                var items = Values.Where(p => p.Value == true).ToList();
+                if (items.Count > 1)
+                    return -1;
+                else
+                    return items[0].Key;
+            }
+        }
+
+        /// <summary>
+        /// All the Values that can be the cell.
+        /// True is a valid value
+        /// False is not a valid value
+        /// </summary>
+        public Dictionary<int, bool> Values { get; set; }
+
+        public Cell()
+        {
+            RefreshDimension();
+        }
+
+        /// <summary>
+        /// Initialize the values by the dimension
+        /// </summary>
+        private void RefreshDimension()
+        {
+            Values.Clear();
+
+            for (int i = 1; i <= dimension; i++)
+                Values.Add(i, true);
+        }
+
+        /// <summary>
+        /// This method reset the value passed inside the dictionary
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>False if value is not inside the range of dimension</returns>
+        public bool ResetValue(int value)
+        {
+            if (value < 1 || value > dimension)
+                return false;
+            Values[value] = false;
+
+            return true;
+        }
+
+    }
+
+}
---
+++ SudokuCore/GroupOfCells.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace SudokuCore.Elements
+{
+    /// <summary>
+    /// Class that indentify a group of cell (they can be row, col or square)
+    /// </summary>
+    public class GroupOfCells
+    {
+        Dictionary<int, Cell> Cells = new Dictionary<int, Cell>();
+
+        public GroupOfCells(Dictionary<int,Cell> groupCells)
+        {
+            Cells = groupCells;
+        }
+
+        /// <summary>
+        /// This method verify if a cell has a certanly value then remove this value from the other cell
+        /// </summary>
+        /// <returns></returns>
+        public bool VerifyCell()
+        {
+            for (int i = 0; i < Cells.Count; i++)
+            {
+                if (Cells[i].CorrectValue > 0)
+                    for (int j = 0; j < Cells.Count; j++)
+                    {
+                        if (j == i)
+                            continue;
+                        Cells[j].ResetValue(Cells[i].CorrectValue);
+                    }
+            }
+            return true;
+        }
+    }
+    public class Row : GroupOfCells
+    {
+        public Row(Dictionary<int, Cell> groupCells) : base(groupCells)
+        {
+        }
+    }
+
+    public class Column : GroupOfCells
+    {
+        public Column(Dictionary<int, Cell> groupCells) : base(groupCells)
+        {
+        }
+    }
+
+    public class Square : GroupOfCells
+    {
+        public Square(Dictionary<int, Cell> groupCells) : base(groupCells)
+        {
+        }
+    }
+
+}
